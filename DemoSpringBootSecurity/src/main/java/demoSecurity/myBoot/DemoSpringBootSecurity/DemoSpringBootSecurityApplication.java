d61900049cbb98cc0043e1d29944fa6db9860ee0
ashimgit <ashimnb87@gmail.com>
2018-09-09 14:18:03 +0530

    Spring Boot Security Initialised

---
+++ DemoSpringBootSecurity/src/main/java/demoSecurity/myBoot/DemoSpringBootSecurity/DemoSpringBootSecurityApplication.java
@@ -0,0 +1,12 @@
+package demoSecurity.myBoot.DemoSpringBootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootSecurityApplication.class, args);
+	}
+}
